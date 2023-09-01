86a12f5b326fb13116ed8325ef861ae455884a45
morz-mamun <morzmamun@gmail.com>
2023-09-02 02:41:46 +0600

    with short btn

--- phero.js
+++ phero.js
@@ -6,97 +6,63 @@ const goToBlog = () => {
 
 // Show category buttons dynamically 
 
-const categoryBtnContainer = document.getElementById('category-btn-container')
-categoryBtnContainer.classList = `flex gap-7 justify-center items-center mt-10`
-categoryBtnContainer.innerHTML = `
-<div>
-<button onclick="getAllCategoryId()" class="btn btn-active hover:bg-[#FF1F3D]">All</button>
-</div>
-<div> 
-<button onclick="getMusicCategoryId()" class="btn btn-active hover:bg-[#FF1F3D]">Music</button></div>
-<div> 
-<button onclick="getComedyCategoryId()" class="btn btn-active hover:bg-[#FF1F3D]">Comedy</button></div>
-<div>
-<button onclick="getDrawingCategoryId()" class="btn btn-active hover:bg-[#FF1F3D]">Drawing</button>
-</div>
+const showCategoryBtn = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+    const allCategory = await response.json()
+    const allData = allCategory.data
 
-`
+    const categoryBtnContainer = document.getElementById('category-btn-container')
+    categoryBtnContainer.classList = `flex gap-2 lg:flex-row lg:gap-7 justify-center items-center mt-10`
+    allData.forEach((data) => {
+        // console.log(data);
+        const btnDiv = document.createElement('div')
+        btnDiv.innerHTML = `
+        <button onclick="getCategoryId('${data.category_id}')" class="btn hover:bg-[#FF1F3D]">${data.category}</button>
+        `
+        categoryBtnContainer.appendChild(btnDiv)
 
-
-// show all category by default 
-const showAllByDefault = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
-    const allData = await res.json()
-    const data = allData.data
-    // console.log(data);
-    const all = data[0].category_id
-    getAllCategoryData(all)
-
+    })
 }
-showAllByDefault()
+showCategoryBtn()
 
 
 
-// (1) show all category by all btn
-const getAllCategoryId = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
-    const allData = await res.json()
-    const data = allData.data
-    const all = data[0].category_id
-    getAllCategoryData(all)
-
+// handler function
+const getCategoryId = (id) => {
+    getAllCategoryData(id)
+    
+    if(id ==='1005'){
+        showDrawingDetails() 
+    } else {
+        const drawingDetails = document.getElementById('drawing-btn-details')
+        drawingDetails.innerHTML = ''
+    }
+    
 }
 
-// (2) show music category by all btn
-const getMusicCategoryId = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
-    const allData = await res.json()
-    const data = allData.data
-    const all = data[1].category_id
-    getAllCategoryData(all)
-
+const handleShortBtn = () => {
+   
 }
-// (3) show comedy category by all btn
-const getComedyCategoryId = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
-    const allData = await res.json()
-    const data = allData.data
-    const all = data[2].category_id
-    getAllCategoryData(all)
-
-}
-
-// (3) show drawing category by all btn
-const getDrawingCategoryId = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
-    const allData = await res.json()
-    const data = allData.data
-    const all = data[3].category_id
-    getAllCategoryData(all)
-    showDrawingDetails()
-
-}
-
-// show modal when click drawing btn
 
 const showDrawingDetails = () => {
-    const showDetails = document.getElementById('drawing-btn-details')
-    showDetails.textContent = ''
-    const drawingDiv = document.createElement('div')
-    drawingDiv.classList = `card w-96 bg-base-100`
-    drawingDiv.innerHTML = `
-        <figure>
-        <img src="images/Icon.png" alt="Shoes" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title text-center">Oops!! Sorry, There is no content here</h2>
-          </div>
-        </div>
-        
-        `
-    showDetails.appendChild(drawingDiv)
+    
+        const showDetails = document.getElementById('drawing-btn-details')
+        showDetails.textContent = ''
+        const drawingDiv = document.createElement('div')
+        drawingDiv.classList = `card w-96 bg-base-100 `
+        drawingDiv.innerHTML = `
+            <figure>
+            <img src="images/Icon.png" alt="Shoes" />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title text-center">Oops!! Sorry, There is no content here</h2>
+              </div>
+            </div>
+            `
+        showDetails.appendChild(drawingDiv)
+    }
+   
 
-}
 
 /* main execution function */
 // getAllCategoryId()
@@ -104,23 +70,41 @@ const showDrawingDetails = () => {
 const getAllCategoryData = async (id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const allCategory = await res.json()
-    
+
     const allData = allCategory.data
 
-// display all card
+    // display all card
 
     const showAllCategoryCard = document.getElementById('card-container')
-
     showAllCategoryCard.textContent = ''
     showAllCategoryCard.classList = `grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 gap-y-10 my-10`
 
     allData.forEach((data) => {
-        console.log(data.authors);
+        // time converter
+
+        const timeConverter = (sec) => {
+            const hours = Math.floor(sec / 3600)
+            const extraSec = sec % 3600
+            const minutes = Math.floor(extraSec / 60)
+        
+            return {hours, minutes}
+        }
+        const {hours, minutes} = timeConverter(data.others.posted_date)
+       
+        
+        // console.log(data.others);
+       
         const cardDiv = document.createElement('div')
-        cardDiv.classList = `card bg-base-100 shadow-xl`
+        cardDiv.classList = `card bg-base-100 shadow-xl relative`
         cardDiv.innerHTML = `
         <figure>
-        <img class="h-52" src="${data.thumbnail}" alt="Picture" />
+        <div>
+        <img class="h-52 w-screen" src="${data.thumbnail}" alt="Picture" />
+        <div class="absolute bottom-28 right-2 bg-black text-white px-2" > ${data.others.posted_date ? `${hours} hrs ${minutes} min ago`  : "" }  
+        </div>
+        </div>
+        
+       
         </figure>
         <div class="flex gap-4 mt-5">
             <div>
@@ -129,7 +113,7 @@ const getAllCategoryData = async (id) => {
             
             <div class="">
             <h2 class="card-title">${data.title}</h2>
-            <p>${data.authors[0].profile_name} <span id="verified-icon">${data.authors[0].verified}</span></p> 
+            <p class="flex gap-2" >${data.authors[0].profile_name} <span">${data.authors[0].verified ? " <img src='images/fi_10629607.png' alt=''>" : ""}</span></p> 
             <p> <span>${data.others.views}</span> views</p>
             </div>
         </div>
@@ -139,3 +123,34 @@ const getAllCategoryData = async (id) => {
 
     })
 }
+
+// show all category by default 
+const showAllCategoryCardByDefault = (id) => {
+    getAllCategoryData(id)
+}
+showAllCategoryCardByDefault(1000)
+
+// short handler 
+
+
+
+// const getDataID = async() => {
+//     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+//     const allCategory = await response.json()
+//     const allData = allCategory.data
+//     allData.forEach((data)=> {
+//         const id = data.category_id
+//         dataById(id)
+//     })
+    
+// }
+
+// const dataById = async(id)=>{
+//     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+//     const allData = await response.json()
+//     const datas = allData.data
+//     const views = datas.forEach(data=> data.others.views)
+    
+// }
+
+handleShortBtn()
